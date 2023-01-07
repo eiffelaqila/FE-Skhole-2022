93260c0f6a82df53610a92dac55707d65103d90e
NabilaFebriani <13421033@mahasiswa.itb.ac.id>
2023-01-07 22:23:22 +0700

    finishing design and dropdown filter

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import * as React from 'react'
 import {
-    Box, Center, Image, Container, Stack, Flex, Heading, Text, Input, InputRightAddon, InputGroup, IconButton, layout, Select
+    Box, Image, Stack, Flex, Heading, Text, Select
 } from '@chakra-ui/react'
 import Head from 'next/head'
 import Footer from '../layout/Footer'
 import Jumbotron from '../components/home/jumbotron'
 
+     
 export default function Home(): JSX.Element {
+
+    const[value, setValue] = React.useState('VII');
+    const handleChange = (event: { target: { value: React.SetStateAction<string> } }) => {
+
+        setValue(event.target.value);
+     
+      };
+
     return (
         <>
             <Head>
@@ -18,34 +27,58 @@ export default function Home(): JSX.Element {
                 minHeight="100vh" 
                 bgColor="#373360" 
                 transform="auto"
+                css={`
+                    background: url(static/images/noise-1280-832.svg), #200655;
+                    background-size: cover
+                `}
                 >
 
-                <Jumbotron />
+                <Jumbotron/>
 
             {/* Text */}
-            <Flex justifyContent={'space-between'}>
-                <Heading color="white" padding="35px 35px 0px 35px">
+            <Flex 
+                justifyContent={'space-between'} 
+                padding="50px 0px 0px 0px"
+            >
+                <Heading 
+                    color="white" 
+                    padding="35px 35px 0px 80px" 
+                    fontSize={54}
+                >
                     Rumah <span style={{color:"#FEE56C"}} color="#FEE56C">Belajar</span>
                 </Heading>
                 
-                <Flex flexDirection='column' padding="35px 35px 0px 35px" gap={2} justifyContent="flex-end">
+                <Flex 
+                    flexDirection='column' 
+                    padding="35px 35px 0px 35px" 
+                    gap={2} 
+                    justifyContent="flex-end" 
+                    alignContent='flex-end'
+                >
                     
                     <Text 
                         color={'white'} 
                         fontFamily='livvic' 
-                        fontSize={21}>
+                        fontSize={21}
+                        padding="0px 65px 0px 0px"
+                        textAlign={'right'}
+                    >
                             <b>Kamu kelas berapa?</b>
                     </Text>
 
-                    <Stack>
+                    <Stack alignItems='flex-end' padding="0px 65px 0px 0px">
+                        
+                        
                         <Select 
-                            placeholder='Pilih Kelas' 
+                            placeholder='' 
                             variant='filled' 
                             bg='#FEE56C' 
                             color="#500D7D" 
                             fontFamily={"inter"} 
                             rounded={20} 
                             maxWidth={'150px'}
+                            value={value}
+                            onChange={handleChange}
                         >
                             <option value='VII'>Kelas VII</option>
                             <option value='VIII'>Kelas VIII</option>
@@ -53,8 +86,9 @@ export default function Home(): JSX.Element {
                             <option value='X'>Kelas X</option>
                             <option value='XI'>Kelas XI</option>
                             <option value='XII'>Kelas XII</option>
-
+                            
                         </Select>
+                        
                     </Stack>
                      
                     
@@ -62,143 +96,166 @@ export default function Home(): JSX.Element {
                 
             </Flex>
             <div>
-            <Box padding={10}>
-                <Flex flexWrap={'wrap'} gap= "30px">
-                    {/*draft design frame 3006 (href nanti di-update kalo udah ada)*/}
-                    <a href='/rumah-belajar'>
+            <Box padding={20} minHeight='600px'>
+                <Flex flexWrap={'wrap'} gap= "40px">
+
+                    {/*draft design frame 3006 (href sementara tp pagesnya blm ada)*/}
+                    <Image 
+                    src="static/images/neptune.svg" 
+                    position="absolute" 
+                    maxWidth="353px"
+                    top="60%" 
+                    right="10%"
+                    zIndex={-1} 
+                    />
+
+                    <a href={`/rumah-belajar/Matematika/${value}`}>
                         <Flex 
-                        h={45} 
-                        alignItems={'center'} 
                         justifyContent={'center'} 
                         flexDirection='column' 
-                        height='110%' 
-                        width='180px'
+                        height='250px' 
+                        width='200px'
                         padding={3}
                         bg="white"
-                        rounded={20}
-                        gap = '10px'
+                        rounded={10}
+                        gap = '27px'
                         >
                             <div>
-                                <Flex>
+                                <Flex alignItems={'center'} justifyContent={'center'} paddingTop={2}>
                                     <Image src='static/images/Matematika.svg'/>
                                 </Flex>
+                                
                             </div>
-                            <Flex>
+                            <Flex flexDirection='column' gap={2} alignItems={'flex-start'} paddingLeft={5}>
                                 <Flex>
-                                    <p><b> Matematika </b> <br/> Kelas XX </p>
+                                    <Image src='static/images/Line.svg'/>
                                 </Flex>
+                                <Text color={'#500D7D'}>
+                                    <p><b> Matematika </b> <br/> <span style={{color:"#000000"}}>Kelas {value}</span> </p>
+                                </Text>
                             </Flex>
                         </Flex>
                     </a>
-                    <a href='/rumah-belajar'>
+                    <a href={`/rumah-belajar/Biologi/${value}`}>
                         <Flex 
-                        h={45} 
-                        alignItems={'center'} 
                         justifyContent={'center'} 
                         flexDirection='column' 
-                        height='110%' 
-                        width='180px'
-                        padding={5}
+                        height='250px' 
+                        width='200px'
+                        padding={3}
                         bg="white"
-                        rounded={20}
-                        gap = '10px'
+                        rounded={10}
+                        gap = '27px'
                         >
                             <div>
-                                <Flex>
+                                <Flex alignItems={'center'} justifyContent={'center'} paddingTop={2}>
                                     <Image src='static/images/Biologi.svg'/>
                                 </Flex>
+                                
                             </div>
-                            <Flex>
+                            <Flex flexDirection='column' gap={2} alignItems={'flex-start'} paddingLeft={5}>
                                 <Flex>
-                                    <p><b> Biologi </b> <br/> Kelas XX </p>
+                                    <Image src='static/images/Line.svg'/>
                                 </Flex>
+                                <Text color={'#500D7D'}>
+                                    <p><b> Biologi </b> <br/> <span style={{color:"#000000"}}>Kelas {value}</span> </p>
+                                </Text>
                             </Flex>
                         </Flex>
                     </a>
-                    <a href='/rumah-belajar'>
+                    <a href={`/rumah-belajar/Kimia/${value}`}>
                         <Flex 
-                        h={45} 
-                        alignItems={'center'} 
                         justifyContent={'center'} 
                         flexDirection='column' 
-                        height='110%' 
-                        width='180px'
-                        padding={5}
+                        height='250px' 
+                        width='200px'
+                        padding={3}
                         bg="white"
-                        rounded={20}
-                        gap = '10px'
+                        rounded={10}
+                        gap = '27px'
                         >
                             <div>
-                                <Flex>
+                                <Flex alignItems={'center'} justifyContent={'center'} paddingTop={2}>
                                     <Image src='static/images/Kimia.svg'/>
                                 </Flex>
+                                
                             </div>
-                            <Flex>
+                            <Flex flexDirection='column' gap={2} alignItems={'flex-start'} paddingLeft={5}>
                                 <Flex>
-                                    <p><b> Kimia </b> <br/> Kelas XX </p>
+                                    <Image src='static/images/Line.svg'/>
                                 </Flex>
+                                <Text color={'#500D7D'}>
+                                    <p><b> Kimia </b> <br/> <span style={{color:"#000000"}}>Kelas {value}</span> </p>
+                                </Text>
                             </Flex>
                         </Flex>
                     </a>
-                    <a href='/rumah-belajar'>
+                    <a href={`/rumah-belajar/Fisika/${value}`}>
                         <Flex 
-                        h={45} 
-                        alignItems={'center'} 
                         justifyContent={'center'} 
                         flexDirection='column' 
-                        height='110%' 
-                        width='180px'
-                        padding={5}
+                        height='250px' 
+                        width='200px'
+                        padding={3}
                         bg="white"
-                        rounded={20}
-                        gap = '10px'
+                        rounded={10}
+                        gap = '27px'
                         >
                             <div>
-                                <Flex>
+                                <Flex alignItems={'center'} justifyContent={'center'} paddingTop={2}>
                                     <Image src='static/images/Fisika.svg'/>
                                 </Flex>
+                                
                             </div>
-                            <Flex>
+                            <Flex flexDirection='column' gap={2} alignItems={'flex-start'} paddingLeft={5}>
                                 <Flex>
-                                    <p><b> Fisika </b> <br/> Kelas XX </p>
+                                    <Image src='static/images/Line.svg'/>
                                 </Flex>
+                                <Text color={'#500D7D'}>
+                                    <p><b> Fisika </b> <br/> <span style={{color:"#000000"}}>Kelas {value}</span> </p>
+                                </Text>
                             </Flex>
                         </Flex>
                     </a>
-                    <a href='/rumah-belajar'>
+                    <a href={`/rumah-belajar/BahasaIndonesia/${value}`}>
                         <Flex 
-                        h={45} 
-                        alignItems={'center'} 
                         justifyContent={'center'} 
                         flexDirection='column' 
-                        height='110%' 
-                        width='180px'
-                        padding={5}
+                        height='250px' 
+                        width='200px'
+                        padding={3}
                         bg="white"
-                        rounded={20}
-                        gap = '10px'
+                        rounded={10}
+                        gap = '27px'
                         >
                             <div>
-                                <Flex>
+                                <Flex alignItems={'center'} justifyContent={'center'} paddingTop={2}>
                                     <Image src='static/images/BahasaIndonesia.svg'/>
                                 </Flex>
+                                
                             </div>
-                            <Flex>
+                            <Flex flexDirection='column' gap={2} alignItems={'flex-start'} paddingLeft={5}>
                                 <Flex>
-                                    <p><b> Bahasa Indonesia </b> <br/> Kelas XX </p>
+                                    <Image src='static/images/Line.svg'/>
                                 </Flex>
+                                <Text color={'#500D7D'}>
+                                    <p><b> Bahasa Indonesia </b> <br/> <span style={{color:"#000000"}}>Kelas {value}</span> </p>
+                                </Text>
                             </Flex>
                         </Flex>
-                    </a>                 
+                    </a>    
+                              
                 </Flex>
              
             </Box>
             </div>
 
-
-            
             </Box>
-            <Footer/>            
+            
+            <Footer/> 
+
+
+
         </>
     )
 }
